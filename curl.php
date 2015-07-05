fa892074bf961af5f5d0af0b8146f5ce2df37f51
Shailender Singh <gatuteck@gmail.com>
2015-07-05 09:46:03 +0530

    Curl Function with text log
    $url will hold the url to scrap and if there is any error then
    logmessage function will hold that log

---
+++ curl.php
@@ -0,0 +1,56 @@
+<?php
+# Author: Shailender Singh
+######################################### 1st Question Solution ####################################
+$url="https://mobile.twitter.com/session/new ";
+//Delay in calling the URL
+define('MinDelayTime', 2);
+define('MaxDelayTime', 8);
+
+function LogMessage($message)
+{	
+	$log_file = "log.txt";	
+    $file = fopen($log_file, 'a');
+    $to_save = date(DATE_COOKIE) . ' -- ' . getmypid() . ' -- ' . $message . "\r\n";
+    if (flock($file, LOCK_EX)) {
+        fputs($file, $to_save);
+    } else {
+        echo 'Cant save log...';
+    }
+
+    echo $to_save . "\r\n";
+    fclose($file);
+}
+// Defining the basic cURL function
+function Get_Curl($url) {
+    //Random sleep or delay in call of page to avoid risk of blocking of IP address
+    sleep(rand(MinDelayTime, MaxDelayTime));
+    LogMessage("URL requested : ".$url);
+
+    $ch = curl_init();  // Initialising cURL
+    curl_setopt($ch, CURLOPT_URL, $url);    // Setting cURL's URL option with the $url variable passed into the function
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, True); // Setting cURL's option to return the webpage data
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, True);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+    $data = curl_exec($ch); // Executing the cURL request and assigning the returned data to the $data variable
+    if(curl_errno($ch)){
+        echo 'Curl error: ' . curl_error($ch);
+    }
+    curl_close($ch);    // Closing cURL
+    return $data;   // Returning html content of site
+}
+?>
+
+<?php
+	################################################ 2 nd Question Solution ######################################
+	$metas = $doc->getElementsByTagName('meta');
+	for ($i = 0; $i < $metas->length; $i++)
+	{
+		$meta = $metas->item($i);
+		if($meta->getAttribute('name') == 'csrf_id')
+			$csrf_id = $meta->getAttribute('content');
+	}
+
+	echo "csrf_id: $csrf_id";
+?>
